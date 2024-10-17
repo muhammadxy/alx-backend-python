03bef192dfdcb45b2d1d6f479cb37c485603fca6
Ayuba Muhammad <76888715+muhammadxy@users.noreply.github.com>
2024-10-17 20:45:26 +0100

    Delete 0x00-MySQL_Advanced directory

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,8 +0,0 @@
--- Write a SQL script that creates a table users
-
-CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
