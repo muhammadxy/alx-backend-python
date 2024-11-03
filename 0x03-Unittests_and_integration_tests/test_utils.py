116d1f0a81aaf8049da18e36ab26b00ef1056ddf
Ayuba Muhammad <76888715+muhammadxy@users.noreply.github.com>
2024-11-03 05:08:56 +0100

    testing

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+"""
+This module contains the first unit test
+for utils.access_nested_map.
+"""
+
+import unittest
+from unittest.mock import patch
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    TestAccessNestedMap inherits from unittest.
+    TestCase to test a utils.access_nested_map.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """ Tests if access_nested_map method returns what it is supposed to.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Tests if access_nested_map method raise an exception.
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    TestAccessNestedMap inherits from unittest. TestCase to test
+    utils.get_json.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Tests if get_json method returns the expected result.
+        """
+        with patch('requests.get') as mock:
+            mock.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mock.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize inherits from unittest. TestCase to test
+    utils.memoize.
+    """
+
+    def test_memoize(self):
+        """
+        Tests if memoize returns the correct result after a_property
+        is called twice.
+        """
+        class TestClass:
+            """ TestClass to test utils.memoize."""
+
+            def a_method(self):
+                """Method that returns 42"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """Method that returns a_method"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            tc = TestClass()
+            self.assertEqual(tc.a_property, mock.return_value)
+            self.assertEqual(tc.a_property, mock.return_value)
+            mock.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
